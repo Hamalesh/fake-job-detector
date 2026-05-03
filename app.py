a2ba0ecf33071518c38beba0420024332e617dc3
Hamalesh <hamaleshkumar6012@gmail.com>
2026-05-03 23:54:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+import joblib
+import numpy as np
+import scipy.sparse
+from preprocess import preprocess_text
+
+app = Flask(__name__)
+
+model = joblib.load('Models/xgboost_model.pkl')
+vectorizer = joblib.load('Models/tfidf_vectorizer.pkl')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.form['job_text']
+
+    clean = preprocess_text(text)
+    text_vec = vectorizer.transform([clean])
+
+    meta = np.array([[0, 0, 0, len(text)]])
+    X = scipy.sparse.hstack([text_vec, meta])
+
+    prediction = model.predict(X)[0]
+    prob = model.predict_proba(X)[0][1]
+
+    if prediction == 1:
+        result = f"Fake Job ❌ (Confidence: {round(prob*100,2)}%)"
+    else:
+        result = f"Legitimate Job ✅ (Confidence: {round((1-prob)*100,2)}%)"
+
+    return render_template('index.html', result=result)   # 👈 INSIDE FUNCTION
+
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=5000)
---
+++ preprocess.py
@@ -0,0 +1,34 @@
+print("FILE RUNNING")
+
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
+
+def preprocess_text(text):
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    words = nltk.word_tokenize(text)
+
+    processed_words = []
+    for word in words:
+        if word not in stop_words:
+            processed_words.append(lemmatizer.lemmatize(word))
+
+    return " ".join(processed_words)
+
+if __name__ == "__main__":
+    sample = "Fake job offer high salary no experience"
+    print(preprocess_text(sample))
+def combine_fields(row):
+    fields = [
+        str(row.get('title', '')),
+        str(row.get('company_profile', '')),
+        str(row.get('description', '')),
+        str(row.get('requirements', '')),
+        str(row.get('benefits', ''))
+    ]
+    return " ".join(fields)
