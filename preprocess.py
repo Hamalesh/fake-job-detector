ddd7b034cd91aeb4a115d483a07c9a023c22c3cc
Hamalesh <hamaleshkumar6012@gmail.com>
2026-05-04 07:14:23 +0530

    Refactor preprocess.py by removing unused functions
    Removed the safe download function and combined field function from the preprocess.py file. The main preprocessing logic remains intact.

--- preprocess.py
+++ preprocess.py
@@ -1,22 +1,7 @@
-print("FILE RUNNING")
-
 import nltk
 import string
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-
-# Safe download (only if missing)
-def safe_nltk_download():
-    try:
-        nltk.data.find('tokenizers/punkt')
-        nltk.data.find('corpora/stopwords')
-        nltk.data.find('corpora/wordnet')
-    except LookupError:
-        nltk.download('punkt')
-        nltk.download('stopwords')
-        nltk.download('wordnet')
-
-safe_nltk_download()
 
 lemmatizer = WordNetLemmatizer()
 STOPWORDS = set(stopwords.words('english'))
@@ -32,13 +17,3 @@ def preprocess_text(text):
             processed_words.append(lemmatizer.lemmatize(word))
 
     return " ".join(processed_words)
-
-def combine_fields(row):
-    fields = [
-        str(row.get('title', '')),
-        str(row.get('company_profile', '')),
-        str(row.get('description', '')),
-        str(row.get('requirements', '')),
-        str(row.get('benefits', ''))
-    ]
-    return " ".join(fields)
